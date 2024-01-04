f803d6b342dd97d5f1b600a1876c0d60a0b6cd08
Luis Plaza <lplazaalvarez@gmail.com>
2024-01-04 02:26:29 -0300

    Improves in table

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import dash
-from dash import dcc, html, Input, Output, Dash, dash_table
-import dash_bootstrap_components as dbc
+from dash import dcc, html, Input, Output, dash_table
 import pandas as pd
 import plotly.express as px
 external_stylesheets = [
@@ -9,8 +8,7 @@ external_stylesheets = [
             "https://fonts.googleapis.com/css2?"
             "family=Lato:wght@400;700&display=swap"
         ),
-        "rel": "stylesheet",
-        
+        "rel": "stylesheet",        
     },'https://codepen.io/chriddyp/pen/bWLwgP.css',
     "https://platform.linkedin.com/in.js",
 
@@ -43,6 +41,9 @@ app.layout = html.Div([
             html.P(children=('Dashboard de la calidad fisicoquímica del agua potable en las diferentes comunas de Chile, de acuerdo a la información reportada por las empresas sanitarias. (SISS,2023). Creado por: Luis Plaza A.'),className='header-description')
         ])],
                 className="header"),
+    html.Div([
+        html.Script(**{"data-url": "https://platform.linkedin.com/in.js"}, type="IN/Share")
+        ]),
     html.Div(
         children=[
             html.Div(
@@ -101,7 +102,7 @@ app.layout = html.Div([
                         
         ],className="wrapper",
         ),
-    html.Div([html.Script(**{"data-url": "https://platform.linkedin.com/in.js"}, type="IN/Share")])
+    
 ])
 
 
@@ -116,6 +117,8 @@ app.layout = html.Div([
     Input('date-range', 'end_date'),
 )
 def update_graph(city, parameter,start_date,end_date):
+    if city==[]:
+        city=['COPIAPO']
     a=[(lambda x: df['Comuna']==x)(x) for x in city]
     mask=a[0]
     for i in range(1,len(city)):
@@ -126,16 +129,13 @@ def update_graph(city, parameter,start_date,end_date):
     try:
         filtered_data['Valor']=filtered_data['Valor'].astype(float)
         filtered_data.sort_index(inplace=True)
-        #print(filtered_data)
         if filtered_data.empty:
             print('No hay valores para estas condiciones')
         else:
             line_plot_fig = px.line(filtered_data, x=filtered_data.index.values, y='Valor',
                                     color='Comuna',title=f'{parameter}')
-            #print('2______________________')
             histogram_fig = px.histogram(filtered_data, x='Valor',color='Comuna',opacity=0.5, 
                                         histnorm='percent', title=f'{parameter} - Histograma')
-            #print('3______________________')
             if parameter in lims.keys():
                 line_plot_fig.add_hline(y=lims[parameter]['lim_max'], line_dash="dash",
                                     line_color="red", annotation_text="Límite Superior", 
@@ -150,7 +150,6 @@ def update_graph(city, parameter,start_date,end_date):
                                     line_color="red", annotation_text="Límite Inferior", 
                                     annotation_position="top")
             else:
-                #print('3______', filtered_data['Limite'].unique())
                 line_plot_fig.add_hline(y=float(filtered_data['Limite'].unique()), line_dash="dash",
                                         line_color="red", annotation_text="Límite Superior", 
                                         annotation_position="bottom right")
@@ -158,14 +157,21 @@ def update_graph(city, parameter,start_date,end_date):
                 histogram_fig.add_vline(x=float(filtered_data['Limite'].unique()), line_dash="dash",
                                         line_color="red", annotation_text="Límite Superior", 
                                         annotation_position="top")
-            #print('4______________________')
             df2=filtered_data.groupby('Comuna')['Valor'].describe().T.round(2)
-            #print(df2)
-            df2=df2.reset_index()
-            #df2.loc[len(df2.index)]=['Last',]
+            df2=df2.reset_index(names='Variable')
             data=df2.to_dict('records')
-            #print(data)
-    except:
+            NEW_NAMES=['Conteo','Media','Desv.Est','Min','Q1','Q2','Q3','Max']
+            for i in range(len(data)): 
+                data[i]['Variable']=NEW_NAMES[i]
+            aux={'Variable':'Last Value'}
+            aux2={'Variable':'Last Date'}
+            for Com in city:
+                mask2=filtered_data['Comuna']==Com
+                aux[Com]=filtered_data[mask2]['Valor'].iloc[-1].round(2)
+                aux2[Com]=filtered_data[mask2].index[-1].strftime("%b-%Y")
+            data.append(aux)
+            data.append(aux2)
+    except Exception:
         print('Problemas durante la actualización')
     return line_plot_fig, histogram_fig, data
 
